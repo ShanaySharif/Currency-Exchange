246dfd01265ca84b1dcbe5c473f19c76e85a5c84
ShanaySharif <Shanay.uw17@gmail.com>
2023-07-02 17:56:10 -0400

    variables directly to strings.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-//Buisness Logic 
+//Buisness Logic
 
 function getConversion(number, currencyToConvertTo) {
   let request = new XMLHttpRequest();
   const url = `https://v6.exchangerate-api.com/v6/f194a6e559fb7e78773e8f2d/latest/USD`;
-  
-  request.addEventListener("loadend", function() {
+
+  request.addEventListener("loadend", function () {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
       printElements(response, number, currencyToConvertTo);
@@ -12,11 +12,18 @@ function getConversion(number, currencyToConvertTo) {
       printError(request, currencyToConvertTo);
     }
   });
-  
+
   request.open("GET", url, true);
   request.send();
 }
 
-
 //UI Logic
 
+function printElements(apiResponse, number, currencyToConvertTo) {
+  const convertedNumber =
+    apiResponse.conversion_rates[currencyToConvertTo] * number;
+  const roundedConvertedNumber = Math.round(convertedNumber * 100) / 100;
+  document.querySelector(
+    "#showResponse"
+  ).innerText = `$${number} in USD converted to ${currencyToConvertTo} is $${roundedConvertedNumber}`;
+}
