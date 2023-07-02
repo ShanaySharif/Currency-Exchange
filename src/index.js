ee48b1be8884ec037f96f49001ecde2994ace664
ShanaySharif <Shanay.uw17@gmail.com>
2023-07-02 17:27:43 -0400

    Buisness logic complete

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-//let url = `https://v6.exchangerate-api.com/v6/YOUR_API_KEY/latest/${fromCurrency.value}`;
+//Buisness Logic 
+
+function getConversion(number, currencyToConvertTo) {
+  let request = new XMLHttpRequest();
+  const url = `https://v6.exchangerate-api.com/v6/f194a6e559fb7e78773e8f2d/latest/USD`;
+  
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, number, currencyToConvertTo);
+    } else {
+      printError(request, currencyToConvertTo);
+    }
+  });
+  
+  request.open("GET", url, true);
+  request.send();
+}
+
+
+//UI Logic
 
